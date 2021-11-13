6f069791bad3710b25d110aa3c33e75293dde3c6
detrasmental <detra.gordon76@gmail.com>
2021-11-13 11:25:30 -0500

    added list of employees

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+    VALUES
+    ('Steve', 'Rogers', 1, 1)
+    ('Tony', 'Stark', 1 , 2),
+    ('Thor', 'Odinson', null, 3),
+    ('Bruce', 'Banner', null, 4),
+    ('Natasha', 'Romanoff', null, 5),
+    ('Clint', 'Barton', null, 6),
+    ('Wanda', 'Maximoff', null, 7),
+    ('Sam', 'Wilson', null, 8),
+    ('Scott', 'Lang', null, 9),
+    ('James', 'Rhodes', null, 10),
+;
+	
