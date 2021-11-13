4364f39eb5e063b59c31ae5314636ffcb5aa09d4
detrasmental <detra.gordon76@gmail.com>
2021-11-13 11:37:13 -0500

    fleshed out schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,21 @@
+INSERT INTO department(department_name)
+VALUES
+("God/Goddess"), 
+("Gadget User"), 
+("Magic User"), 
+("Powerhouse"), 
+("Shapeshifter");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+("Leader", 100000, 1), 
+("Second in Command", 75000, 1), 
+("Support", 50000, 3), 
+("On Call", 25000, 4);
+
+
+
+
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
     VALUES
     ('Steve', 'Rogers', 1, 1)
